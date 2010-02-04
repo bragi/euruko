7db2ec62731196881b8035bf6106b16854c4309c
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2010-02-04 18:44:38 +0100

    Added constrains to user model

--- db/migrate/20100203205958_create_users.rb
+++ db/migrate/20100203205958_create_users.rb
@@ -1,14 +1,14 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :email
-      t.string :crypted_password
-      t.string :password_salt
-      t.string :persistence_token
+      t.string :email, :nul => false
+      t.string :crypted_password, :null => false
+      t.string :password_salt, :null => false
+      t.string :persistence_token, :null => false
       t.string :single_access_token
       t.string :perishable_token
-      t.integer :login_count
-      t.integer :failed_login_count
+      t.integer :login_count, :null => false, :default => 0
+      t.integer :failed_login_count, :null => false, :default => 0
       t.datetime :last_request_at
       t.datetime :current_login_at
       t.datetime :last_login_at
--- db/schema.rb
+++ db/schema.rb
@@ -13,13 +13,13 @@ ActiveRecord::Schema.define(:version => 20100203205958) do
 
   create_table "users", :force => true do |t|
     t.string   "email"
-    t.string   "crypted_password"
-    t.string   "password_salt"
-    t.string   "persistence_token"
+    t.string   "crypted_password",                   :null => false
+    t.string   "password_salt",                      :null => false
+    t.string   "persistence_token",                  :null => false
     t.string   "single_access_token"
     t.string   "perishable_token"
-    t.integer  "login_count"
-    t.integer  "failed_login_count"
+    t.integer  "login_count",         :default => 0, :null => false
+    t.integer  "failed_login_count",  :default => 0, :null => false
     t.datetime "last_request_at"
     t.datetime "current_login_at"
     t.datetime "last_login_at"
