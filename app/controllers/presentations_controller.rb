9efe40627d676b6d2f1cf8e3cb28f4af564c3693
kazjote <kazjote@ragnarson.com>
2010-02-18 09:28:57 +0100

    Implemented Editing presentation submission story

--- app/controllers/presentations_controller.rb
+++ app/controllers/presentations_controller.rb
@@ -2,10 +2,10 @@ class PresentationsController < ApplicationController
   resource_controller
   
   before_filter :require_user
-  before_filter :require_owner, :only => :show
+  before_filter :require_owner, :only => [:show, :update]
   
-  actions :new, :show, :create
+  actions :new, :show, :create, :edit, :update
   
   create.before {object.user = current_user}
-  create.wants.html {redirect_to current_user}
+  [create, update].each {|action| action.wants.html {redirect_to current_user}}
 end
