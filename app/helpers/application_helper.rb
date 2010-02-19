bbd7f7eeac6b636bfdea66582022882df52d1255
Aleksander Dabrowski <aleks@kumulator.pl>
2010-02-19 01:05:35 +0100

    layout initialization

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
   def admin_area?
     params[:controller] =~ /administration/
   end
+
+  def menu_to(name, options = {}, html_options = {}, &block)
+    content = link_to(name, options, html_options, &block)
+    if current_page?(options)
+      "<li class=\"selected\">#{content}</li>"
+    else
+      "<li>#{content}</li>"
+    end
+  end
 end
